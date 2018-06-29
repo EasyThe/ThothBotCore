156e7a61d41717a2d4afd7817f312e99731ab87e
Seyhan Halil <f3n1xx@mail.bg>
2018-06-29 04:47:55 +0300

    Added InMemoryStorage.cs IDataStorage.cs and Unity.cs

--- ThothBotCore/Program.cs
+++ ThothBotCore/Program.cs
@@ -6,6 +6,7 @@ namespace ThothBotCore
     {
         private static void Main()
         {
+            Unity.RegisterTypes();
             Console.WriteLine("Hello, Discord!");
         }
     }
---
+++ ThothBotCore/Storage/IDataStorage.cs
@@ -0,0 +1,11 @@
+﻿namespace ThothBotCore.Storage
+{
+    public interface IDataStorage
+    {
+        void StoreObject(object obj, string key);
+
+        T RestoreObject<T>(string key);
+
+
+    }
+}
