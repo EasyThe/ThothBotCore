0670bbb06f97922c94a0df48e8355f7c101c2635
Seyhan Halil <f3n1xx@mail.bg>
2018-06-29 03:40:03 +0300

    first commit

---
+++ ThothBotCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ThothBotCore
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Any() && args[0] == "--version") Console.WriteLine("0.0.1");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
