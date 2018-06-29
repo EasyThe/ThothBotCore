30b65835617a917f77fa2a5804325b6549c9398b
Seyhan Halil <f3n1xx@mail.bg>
2018-06-29 03:48:02 +0300

    Update Program.cs and Readme
    Changed "Hello World!" to "Hello, Discord!" and removed the " from Readme file.

--- ThothBotCore/Program.cs
+++ ThothBotCore/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Linq;
 
 namespace ThothBotCore
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            if (args.Any() && args[0] == "--version") Console.WriteLine("0.0.1");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, Discord!");
         }
     }
 }
