4cb6737145d14e69ff9dcf021ae1930004b31595
Seyhan Halil <f3n1xx@mail.bg>
2018-06-29 03:49:37 +0300

    Revert "Update Program.cs and Readme"
    This reverts commit 30b65835617a917f77fa2a5804325b6549c9398b.

--- ThothBotCore/Program.cs
+++ ThothBotCore/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Linq;
 
 namespace ThothBotCore
 {
     internal class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Discord!");
+            if (args.Any() && args[0] == "--version") Console.WriteLine("0.0.1");
+            Console.WriteLine("Hello World!");
         }
     }
 }
